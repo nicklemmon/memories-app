c72e7992461cae4fe4b3f2a7d2ee4fcaf64db861
Nick Lemmon <nick.lemmon@gmail.com>
2018-05-26 20:23:05 -0400

    Add login form component and users model

---
+++ src/components/FormLogIn.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import axios from 'axios'
+
+import FormWrapper from './FormWrapper.jsx'
+import FormGroup from './FormGroup.jsx'
+import ButtonWrapper from './ButtonWrapper.jsx'
+import Button from './Button.jsx'
+
+class FormLogIn extends React.Component {
+  constructor( props ) {
+    super( props )
+
+    this.state = {
+      hasAlert: false,
+      alertType: null,
+      alertContent: null
+    }
+  }
+
+  handleSubmit( e ) {
+    e.preventDefault();
+    
+    console.log( 'submit!' )
+  }
+
+
+
+  render() {
+    return (
+      <FormWrapper
+        hasAlert={ this.state.hasAlert }
+        alertType={ this.state.alertType }
+        alertContent={ this.state.alertContent }
+      >
+        <FormGroup
+          label='Username'
+          type='email'
+          id='username'
+        />
+
+        <FormGroup
+          label='Password'
+          type='password'
+          id='password'
+        />
+
+        <ButtonWrapper>
+          <Button
+            type='primary'
+            content='Login'
+            fullWidth='true'
+          />
+        </ButtonWrapper>
+      </FormWrapper>
+    )
+  }
+}
+
+export default FormLogIn
