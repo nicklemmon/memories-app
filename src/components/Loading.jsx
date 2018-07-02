5fd0d738b7ca499f7c8599818107906c58ba2393
Nick Lemmon <nick.lemmon@gmail.com>
2018-07-02 19:52:35 -0400

    Add loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Loading extends React.Component {
+  render() {
+    return (
+      <div className='Loading'>
+        <span className='Loading-text'>Loading...</span>
+      </div>
+    )
+  }
+}
+
+export default Loading
