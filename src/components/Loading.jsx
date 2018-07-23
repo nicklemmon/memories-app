cd4633ea5b83912b95cd0c219f64511313734f04
Nick Lemmon <nick.lemmon@gmail.com>
2018-07-22 20:39:56 -0400

    Fix attribute name issue

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -6,16 +6,16 @@ class Loading extends React.Component {
   render() {
     return (
       <span className='Loading' aria-label='Loading'>
-        <span class='Loading-wrapper'>
-          <span class='Loading-dot Loading-dot--1' role='presentation'></span>
+        <span className='Loading-wrapper'>
+          <span className='Loading-dot Loading-dot--1' role='presentation'></span>
 
-          <span class='Loading-dot Loading-dot--2' role='presentation'></span>
+          <span className='Loading-dot Loading-dot--2' role='presentation'></span>
 
-          <span class='Loading-dot Loading-dot--3' role='presentation'></span>
+          <span className='Loading-dot Loading-dot--3' role='presentation'></span>
 
-          <span class='Loading-dot Loading-dot--4' role='presentation'></span>
+          <span className='Loading-dot Loading-dot--4' role='presentation'></span>
 
-          <span class='Loading-dot Loading-dot--5' role='presentation'></span>
+          <span className='Loading-dot Loading-dot--5' role='presentation'></span>
         </span>
       </span>
     )
