953de581e4e61c2dfd9ccb6b7c2e3f021191f57f
Nick Lemmon <nick.lemmon@gmail.com>
2019-02-10 22:05:27 -0500

    Remove Parse invocations

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import React from 'react'
-import Parse from 'parse';
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import DefaultLayout from './layouts/DefaultLayout.jsx'
-
-Parse.serverURL = process.env.REACT_APP_API_BASE_URL;
-Parse.initialize(
-  process.env.REACT_APP_APPLICATION_ID,
-  process.env.REACT_APP_API_KEY
-);
 
 class App extends React.Component {
   render() {
