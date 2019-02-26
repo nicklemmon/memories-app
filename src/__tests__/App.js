03d05b345974a9bb9a3144ad3c781b200645f3da
Nick Lemmon <nick.lemmon@gmail.com>
2019-02-25 21:16:58 -0500

    Install enzyme, add succeeding test

---
+++ src/__tests__/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import App from '../App.js'
+
+describe( '<App/>', () => {
+  it( 'Renders without crashing', () => {
+    shallow( <App /> );
+  })
+})
