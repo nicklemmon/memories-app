ced59c94cb6034a7c91a717873ec56391de49f1d
Nick Lemmon <nick.lemmon@gmail.com>
2019-12-14 21:49:24 -0500

    Testing commit hook

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import classNames from 'classnames'
-import posed from 'react-pose'
+import React from 'react';
+import classNames from 'classnames';
+import posed from 'react-pose';
 
 import {
   FaCheckCircle,
   FaExclamationCircle,
   FaExclamationTriangle,
-  FaInfoCircle
-} from 'react-icons/fa'
+  FaInfoCircle,
+} from 'react-icons/fa';
 
+import './Alert.css';
 
-import './Alert.css'
 
 const PosedDiv = posed.div({
   visible: {
@@ -19,90 +19,76 @@ const PosedDiv = posed.div({
     scaleX: 1,
     transition: {
       type: 'spring',
-      delay: 300
-    }
+      delay: 300,
+    },
   },
   hidden: {
     opacity: 0,
     scaleY: 0.66,
-    scaleX: 0.925  
-  }
+    scaleX: 0.925,
+  },
 });
 
 class Alert extends React.Component {
-  constructor( props ) {
-    super( props )
+  constructor(props) {
+    super(props);
 
-    this.alertContent = React.createRef()
+    this.alertContent = React.createRef();
 
     this.state = {
-      isHidden: true
-    }
+      isHidden: true,
+    };
 
-    this.show = this.show.bind( this )
-    this.hide = this.hide.bind( this )
+    this.show = this.show.bind(this);
+    this.hide = this.hide.bind(this);
   }
 
   componentDidMount() {
-    this.setState({ isHidden: false })
-    this.alertContent.current.focus()
+    this.setState({ isHidden: false });
+    this.alertContent.current.focus();
   }
 
   show() {
-    this.setState( {
-      isHidden: false
-    })
+    this.setState({
+      isHidden: false,
+    });
   }
 
   hide() {
-    this.setState( {
-      isHidden: true
-    })
+    this.setState({
+      isHidden: true,
+    });
   }
 
   render() {
-    const {
-      type,
-      content,
-      className,
-      cy,
-      ...attributes
-    } = this.props
+    const { type, content, className, cy, ...attributes } = this.props;
 
-    const isHidden = this.state.isHidden
+    const isHidden = this.state.isHidden;
 
     return (
       <PosedDiv
-        className={ classNames( `Alert Alert--${type}`, className ) } 
-        role='alert'
-        pose={ isHidden ? 'hidden' : 'visible' }
-        data-cy={ cy }
-        { ...attributes }
+        className={classNames(`Alert Alert--${type}`, className)}
+        role="alert"
+        pose={isHidden ? 'hidden' : 'visible'}
+        data-cy={cy}
+        {...attributes}
       >
-        <div className='Alert-pre'>
-          { type === 'error' &&
-            <FaExclamationCircle className='Alert-icon'/>
-          }
+        <div className="Alert-pre">
+          {type === 'error' && <FaExclamationCircle className="Alert-icon" />}
 
-          { type === 'success' &&
-            <FaCheckCircle className='Alert-icon'/>
-          }
+          {type === 'success' && <FaCheckCircle className="Alert-icon" />}
 
-          { type === 'attention' &&
-            <FaExclamationTriangle className='Alert-icon'/>
-          }
+          {type === 'attention' && <FaExclamationTriangle className="Alert-icon" />}
 
-          { type === 'info' &&
-            <FaInfoCircle className='Alert-icon'/>
-          }
-      </div>
+          {type === 'info' && <FaInfoCircle className="Alert-icon" />}
+        </div>
 
-      <div className='Alert-content' ref={ this.alertContent } tabIndex='-1'>
-        { content }
-      </div>
-     </PosedDiv>
-    )
+        <div className="Alert-content" ref={this.alertContent} tabIndex="-1">
+          {content}
+        </div>
+      </PosedDiv>
+    );
   }
 }
 
-export default Alert
+export default Alert;
