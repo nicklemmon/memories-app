fcbffd8908a73e3f4a37e629ea4d340d37026abd
Nick Lemmon <nick.lemmon@gmail.com>
2019-12-21 14:51:29 -0500

    Reformat file with Prettier

--- src/stores/AppStore.js
+++ src/stores/AppStore.js
@@ -1,15 +1,15 @@
 import { observable, action } from 'mobx'
 
-const AppStore = observable( {
-  isLoading: false
-} )
+const AppStore = observable({
+  isLoading: false,
+})
 
-AppStore.setLoading = action( function setLoading() {
-	AppStore.isLoading = true
-} )
+AppStore.setLoading = action(function setLoading() {
+  AppStore.isLoading = true
+})
 
-AppStore.unsetLoading = action( function unsetLoading() {
-	AppStore.isLoading = false
-} )
+AppStore.unsetLoading = action(function unsetLoading() {
+  AppStore.isLoading = false
+})
 
 export default AppStore
