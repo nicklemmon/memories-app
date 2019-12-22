0ad3d2031610d7da5f374230a8901574da827751
Nick Lemmon <nick.lemmon@gmail.com>
2019-12-21 21:01:08 -0500

    Remove remaining app tests, remove Enzyme stuff from setup files

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import App from './App.js'
-
-describe( '<App/>', () => {
-  it( 'Renders without crashing', () => {
-    shallow( <App /> )
-  })
-})
