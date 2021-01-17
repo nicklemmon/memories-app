27803a7fd85ea617e17c79f8380cf65342fcbfe2
Nick Lemmon <nick.lemmon@gmail.com>
2021-01-16 20:35:22 -0500

    Incorporate react-table

--- src/pages/search/components/SearchTable.js
+++ src/pages/search/components/SearchTable.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useQuery } from 'react-query'
+import { useTable } from 'react-table'
 import { getMemories } from 'src/helpers/api'
 import { Alert, MaxWidth, PageLoader, ScreenReaderOnly } from 'src/components'
 import { formatMemories } from 'src/helpers/memories'
@@ -7,6 +8,28 @@ import './SearchTable.css'
 
 export function SearchTable() {
   const { status, data } = useQuery('getMemories', getMemories)
+  const memories = formatMemories(data)
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Title',
+        accessor: 'title',
+      },
+      {
+        Header: 'Summary',
+        accessor: 'summary',
+      },
+      {
+        Header: 'Date',
+        accessor: 'date',
+      },
+    ],
+    [],
+  )
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
+    columns,
+    data: memories,
+  })
 
   if (status === 'loading') return <PageLoader />
 
@@ -30,35 +53,45 @@ export function SearchTable() {
     )
   }
 
-  const memories = formatMemories(data)
-
   return (
-    <table className="Table">
-      <ScreenReaderOnly as="caption">Memories</ScreenReaderOnly>
+    <MaxWidth size="lg">
+      <table className="Table" {...getTableProps()}>
+        <ScreenReaderOnly as="caption">Memories</ScreenReaderOnly>
 
-      <thead className="Table-head">
-        <tr className="Table-row">
-          <th className="Table-headCell">Title</th>
+        <thead className="Table-head">
+          {headerGroups.map(headerGroup => {
+            return (
+              <tr className="Table-row" {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map(column => {
+                  return (
+                    <th className="Table-headCell" {...column.getHeaderProps()}>
+                      {column.render('Header')}
+                    </th>
+                  )
+                })}
+              </tr>
+            )
+          })}
+        </thead>
 
-          <th className="Table-headCell">Summary</th>
+        <tbody className="Table-body" {...getTableBodyProps()}>
+          {rows.map(row => {
+            prepareRow(row)
 
-          <th className="Table-headCell">Date</th>
-        </tr>
-      </thead>
-
-      <tbody className="Table-body">
-        {memories.map(memory => {
-          return (
-            <tr className="Table-row" key={memory.objectId}>
-              <td className="Table-cell">{memory.title}</td>
-
-              <td className="Table-cell">{memory.summary}</td>
-
-              <td className="Table-cell">{memory.date}</td>
-            </tr>
-          )
-        })}
-      </tbody>
-    </table>
+            return (
+              <tr className="Table-row" {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return (
+                    <td className="Table-cell" {...cell.getCellProps()}>
+                      {cell.render('Cell')}
+                    </td>
+                  )
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </MaxWidth>
   )
 }
