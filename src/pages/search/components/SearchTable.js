da8799682eda3d7eae272e8385c8d0eb533bb798
Nick Lemmon <nick.lemmon@gmail.com>
2021-01-16 21:18:54 -0500

    Basic filtering working

--- src/pages/search/components/SearchTable.js
+++ src/pages/search/components/SearchTable.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import { useQuery } from 'react-query'
-import { useTable } from 'react-table'
+import { useTable, useGlobalFilter, useAsyncDebounce } from 'react-table'
 import { getMemories } from 'src/helpers/api'
-import { Alert, MaxWidth, PageLoader, ScreenReaderOnly } from 'src/components'
+import {
+  Alert,
+  Card,
+  CardContent,
+  FormGroup,
+  MaxWidth,
+  PageLoader,
+  ScreenReaderOnly,
+} from 'src/components'
 import { formatMemories } from 'src/helpers/memories'
 import './SearchTable.css'
 
@@ -26,10 +34,30 @@ export function SearchTable() {
     ],
     [],
   )
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
-    columns,
-    data: memories,
-  })
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    setGlobalFilter,
+    state,
+  } = useTable(
+    {
+      columns,
+      data: memories,
+      autoResetGlobalFilter: false,
+      initialState: {
+        globalFilter: '',
+      },
+    },
+    useGlobalFilter,
+  )
+  const handleFilterChange = useAsyncDebounce(value => {
+    setGlobalFilter(value || undefined)
+  }, 200)
+  const [filterValue, setFilterValue] = React.useState(state.globalFilter)
+  const hasRows = Boolean(rows.length)
 
   if (status === 'loading') return <PageLoader />
 
@@ -55,43 +83,67 @@ export function SearchTable() {
 
   return (
     <MaxWidth size="lg">
-      <table className="Table" {...getTableProps()}>
-        <ScreenReaderOnly as="caption">Memories</ScreenReaderOnly>
+      <Card>
+        <CardContent>
+          <FormGroup
+            label="Filter"
+            type="text"
+            id="filter"
+            handleChange={e => {
+              setFilterValue(e.target.value)
+              handleFilterChange(e.target.value)
+            }}
+            value={filterValue}
+            className="Table-filterField"
+          />
 
-        <thead className="Table-head">
-          {headerGroups.map(headerGroup => {
-            return (
-              <tr className="Table-row" {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map(column => {
+          <table className="Table" {...getTableProps()}>
+            <ScreenReaderOnly as="caption">Memories</ScreenReaderOnly>
+
+            <thead className="Table-head">
+              {headerGroups.map(headerGroup => {
+                return (
+                  <tr className="Table-row" {...headerGroup.getHeaderGroupProps()}>
+                    {headerGroup.headers.map(column => {
+                      return (
+                        <th className="Table-headCell" {...column.getHeaderProps()}>
+                          {column.render('Header')}
+                        </th>
+                      )
+                    })}
+                  </tr>
+                )
+              })}
+            </thead>
+
+            <tbody className="Table-body" {...getTableBodyProps()}>
+              {hasRows ? (
+                rows.map(row => {
+                  prepareRow(row)
+
                   return (
-                    <th className="Table-headCell" {...column.getHeaderProps()}>
-                      {column.render('Header')}
-                    </th>
+                    <tr className="Table-row" {...row.getRowProps()}>
+                      {row.cells.map(cell => {
+                        return (
+                          <td className="Table-cell" {...cell.getCellProps()}>
+                            {cell.render('Cell')}
+                          </td>
+                        )
+                      })}
+                    </tr>
                   )
-                })}
-              </tr>
-            )
-          })}
-        </thead>
-
-        <tbody className="Table-body" {...getTableBodyProps()}>
-          {rows.map(row => {
-            prepareRow(row)
-
-            return (
-              <tr className="Table-row" {...row.getRowProps()}>
-                {row.cells.map(cell => {
-                  return (
-                    <td className="Table-cell" {...cell.getCellProps()}>
-                      {cell.render('Cell')}
-                    </td>
-                  )
-                })}
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
+                })
+              ) : (
+                <tr className="Table-row">
+                  <td className="Table-cell" colSpan="100%">
+                    No results found. <span aria-hidden="true">😔</span>
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </CardContent>
+      </Card>
     </MaxWidth>
   )
 }
